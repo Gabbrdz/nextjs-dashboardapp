a70e135be623016d6a52c5085bb30906dc503b89
Gabriela Rodriguez <Gabbrdz@users.noreply.github.com>
2025-08-25 11:53:39 -0600

    feat: add vercel/speed-insights/next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
@@ -9,6 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <SpeedInsights/>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
